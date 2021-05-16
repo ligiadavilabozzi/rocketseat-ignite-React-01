ceaf3a22bac8a848c7fe624fd53b20dd98a0d02f
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-16 17:59:33 -0300

    create component RepositoryItem

---
+++ src/components/RepositiryItem.jsx
@@ -0,0 +1,14 @@
+export function RepositoryItem (){
+    return (
+        <li> {/* li para cada repositório com */}
+        <strong>unform</strong> {/* nome do repositório que puxaremos da api do git hub */}
+        <p>Forms in React</p> {/* descrição do repositório */}
+
+        <a href="">
+            Acessar Respositório
+        </a>
+    </li>
+
+    )
+
+}
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,37 +1,16 @@
-const repositoryName = 'unform2'
+import { RepositoryItem } from "./RepositiryItem"
+
+
 export function RepositoryList(){
     return(
         <section className="repository-list">
             <h1>Lista de repositório</h1>
         
-        <ul> {/* lista não ordenada*/}
-            <li> {/* li para cada repositório com */}
-                <strong>{repositoryName}</strong> {/* nome do repositório que puxaremos da api do git hub */}
-                <p>Forms in React</p> {/* descrição do repositório */}
-
-                <a href="">
-                    Acessar Respositório
-                </a>
-            </li>
-
-            <li> {/* li para cada repositório com */}
-                <strong>unform</strong> {/* nome do repositório que puxaremos da api do git hub */}
-                <p>Forms in React</p> {/* descrição do repositório */}
-
-                <a href="">
-                    Acessar Respositório
-                </a>
-            </li>
-
-            <li> {/* li para cada repositório com */}
-                <strong>unform</strong> {/* nome do repositório que puxaremos da api do git hub */}
-                <p>Forms in React</p> {/* descrição do repositório */}
-
-                <a href="">
-                    Acessar Respositório
-                </a>
-            </li>
-
+        <ul> 
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
         </ul>
         
         </section>
